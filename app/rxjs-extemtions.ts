a4ccab19d54158e611762cf7a9f281f49ae829ec
Deniss <denmantm@inbox.lv>
2017-09-01 14:57:13 +0000

    rem some addons

--- app/rxjs-extemtions.ts
+++ app/rxjs-extemtions.ts
@@ -5,5 +5,5 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 // import 'rxjs/add/operator/first'
-import 'rxjs/add/operator/toPromise';
+//import 'rxjs/add/operator/toPromise';
 //import 'rxjs/add/operator/filter';
